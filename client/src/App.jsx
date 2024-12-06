39599bf535331a1eeae5916ebeab6923bccce833
kinjalk tripathi <kinjalkt3@gmail.com>
2024-12-06 14:06:08 +0530

    setup frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Routing from './router'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='text-rose-400'>askdjas</h1>
-    </>
+    <div>
+      <Routing />
+    </div>
   )
 }
 
---
+++ client/src/router.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Event from './pages/Event';
+import Auth from './pages/Auth';
+
+const Routing = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/events/:id" element={<Event />} />
+        </Routes>
+    )
+}
+
+export default Routing;
